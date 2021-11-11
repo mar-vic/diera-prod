157b3fc2bb1505ca634c36814963b3f8d2715f1e
Marek Vician <project2501g@gmail.com>
2021-11-11 19:56:57 +0100

    Custom urls for apphooked photologue

--- diera/photologue_cms_integration/urls.py
+++ diera/photologue_cms_integration/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import *
+from django.urls import path, re_path
 
-from photologue.views import GalleryArchiveIndexView, GalleryListView
+from photologue.views import GalleryArchiveIndexView, GalleryYearArchiveView
 
 urlpatterns = [
-    url(r'^gallerylist/$',
-        GalleryListView.as_view(),
-        name='photologue-cms-integration-gallery-list'),
-    url(r'',
-        GalleryArchiveIndexView.as_view(),
-        name='pohtologue-cms-integration-gallery-archive-index'),
+    # re_path(r'^',
+    #     GalleryArchiveIndexView.as_view(),
+    #     name='pli-gallery-archive'),
+    # re_path(r'^(?P<year>\d{4})/$',
+    #         GalleryYearArchiveView.as_view(),
+    #         name='pli-gallery-archive-year'),
+    path('',
+         GalleryArchiveIndexView.as_view(),
+         name='pli-gallery-archive'),
+    path('<year>/',
+         GalleryYearArchiveView.as_view(),
+         name='pli-gallery-year-archive')
 ]
